a27eec7c4a945394472bf4dfdad7920460020ef4
darethehair <darethehair@gmail.com>
2015-07-24 09:16:50 -0500

    Update flight_warning.sh

--- flight_warning.sh
+++ flight_warning.sh
@@ -1,1 +1,16 @@
-nc localhost 30003 | python /usr/local/bin/flight_warning.py >> /tmp/flight_warning.out
+#!/bin/bash
+#
+# Parent bash script for flight_warning system.
+#
+# Intended to automatically restart the main bash/python scripts if they crash for whatever reason.
+# Can be made to automatically start during reboot by adding to crontab schedule e.g
+# 
+#	@reboot /usr/local/bin/flight_warning_parent.sh
+#
+
+logger -t $0 "starting flight_warning.sh..."
+until /usr/local/bin/flight_warning.sh; do
+	logger -t $0 "flight_warning.sh crashed so restarting it..."
+    	sleep 1
+done
+
